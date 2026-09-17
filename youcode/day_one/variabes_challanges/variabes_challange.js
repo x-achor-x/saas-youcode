75a85b002489d899b14c7a1ad336c89e95a3c669
X-achor-x <sarajendade@gmail.com~>
2026-09-17 15:44:04 +0200

    laal

---
+++ youcode/day_one/variabes_challanges/variabes_challange.js
@@ -0,0 +1,7 @@
+const   prompt = require("prompt-sync")();
+const   name = prompt("Entrez votre nom complete :");
+const   age = prompt("Entrez votre âge :");
+const   sexe = prompt("Indiquez votre sexe :");
+const   email = prompt("Entrez votre adresse e-mail :");
+
+console.log( name ,'\n' ,age ,'\n',sexe ,'\n',email );
